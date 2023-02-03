63d59e72ef4d1560901586a486e1a11bd8ccbcf9
jiangshanmeta <540118044@qq.com>
2023-02-04 00:52:26 +0800

    fix: add github action

--- config-overrides.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    // The Webpack config to use when compiling your react app for development or production.
-    webpack: function(config, env) {
-        // ...add your webpack config
-        return config;
-    },
-};
